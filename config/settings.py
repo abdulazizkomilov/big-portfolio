79cffc8e2c4c222d41cdd1daef49979e1b57db5b
abulaziz <abdulazizkomilov2001@gmail.com>
2021-08-25 19:13:45 +0500

    login and logout

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     #external
+    'crispy_forms',
     #local
     'accounts',
     'articles',
@@ -129,3 +130,9 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
